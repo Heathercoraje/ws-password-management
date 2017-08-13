431ae5b0282a099be1c6a476f5d67519353f6070
heathercoraje <yhk7830@hotmail.com>
2017-08-13 12:25:49 +0300

    first attempt to solution

--- bcrypt-exercise/index.js
+++ bcrypt-exercise/index.js
@@ -3,9 +3,26 @@
 const bcrypt = require('bcryptjs');
 
 const hashPassword = (password, callback) => {
+
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+      if (err) console.log(err);
+      else {
+        console.log(hash);
+        callback(null, hash)
+      }
+      // Store hash in your password DB.
+    });
+  });
   // use bcrypt to hash the password and return it asynchronously
-  callback(new Error('TODO'));
+  //callback(new Error('TODO'));
 };
+
+
+//bcrypt.hash(password, 10, callback);
+
+
+
 
 const comparePasswords = (password, hashedPassword, callback) => {
   // use bcrypt to compare the passwords and return a boolean asynchronously
